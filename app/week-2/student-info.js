239167390be5c40d02038876d95d3c72ae097c00
Shortink <21148306+Shortink@users.noreply.github.com>
2024-05-13 15:17:47 -0600

    submission

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+
+    return(
+        <div>
+            <p>Elvis</p>
+            <Link href="https://github.com/Shortink/cprg306-assignments/tree/master">Github Link</Link>
+        </div>
+    );
+}
